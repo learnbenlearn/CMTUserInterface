34d05cae46ac8e7a3790cf393a957ca266b83895
Ben <blearn5241@gmail.com>
2022-06-11 22:06:20 -0400

    finished recordFormField component

--- force-app/main/default/lwc/recordForm/recordForm.js
+++ force-app/main/default/lwc/recordForm/recordForm.js
@@ -6,7 +6,7 @@ const INPUT_TYPES = {
     'DATETIME': 'datetime',
     'EMAIL': 'email',
     'DOUBLE': 'number',
-    'PERCENT': 'percent',
+    'PERCENT': 'number',
     'PHONE': 'tel',
     'STRING': 'text',
     'URL': 'url'
@@ -29,31 +29,61 @@ export default class CmtRecordForm extends LightningElement {
             let customField = {};
 
             if(Object.keys(INPUT_TYPES).indexOf(customFieldDescribeResult.DataType) != -1) {
-                customField.componentType = 'Input';
-                customField.isRichText = false;
-                customField.type = INPUT_TYPES[customField.DataType];
+                customField.type = INPUT_TYPES[customFieldDescribeResult.DataType];
 
                 switch(customFieldDescribeResult.DataType) {
                     case 'DOUBLE':
                         customField.formatter = 'decimal';
-                        break;
                     case 'PERCENT':
-                        customField.formatter = 'percent';
+                        customField.formatter = customField.formatter ?? 'percent';
+
+                        if(customFieldDescribeResult.DecimalPlaces) {
+                            customField.componentType = 'Input Decimal Component';
+                            let stepString = '0.';
+                            for(let a = 1; a < customFieldDescribeResult.DecimalPlaces; a++) {
+                                stepString += '0';
+                            }
+                            stepString += '1';
+
+                            customField.step = stepString;
+                        } else if ((customField.formatter === 'percent') && !customField.step) {
+                            customField.componentType = 'Input Decimal Component';
+                            customField.step = '0.01';
+                        } else {
+                            customField.componentType = 'Input Number Component';
+                        }
                         break;
+                    case 'STRING':
+                        customField.componentType = 'Input Text Component';
+                        break;
+                    case 'PHONE':
+                        customField.componentType = 'Input Phone Component';
+                        customField.pattern = '\\([0-9]{3}\\)\\s[0-9]{3}-[0-9]{4}';
+                        customField.messageWhenPatternMismatch = 'Please input a phone number in the format (123) 456-7890.'
+                        break;
+                    default:
+                        customField.componentType = 'Input Component';
                 }
 
             } else if(customFieldDescribeResult.DataType === 'PICKLIST') {
                 customField.componentType = 'Combobox';
-                customField.isRichText = false;
                 customField.options = customFieldDescribeResult.PicklistValues;
             } else if(customFieldDescribeResult.DataType === 'TEXTAREA') {
-                customField.componentType = 'Input Rich Text';
-                customField.isRichText = true;
+                customField.componentType = 'Text Area';
             }
 
+            if(customFieldDescribeResult.MaxLength) {
+                customField.maxLength = customFieldDescribeResult.MaxLength;
+            }
+
+            if(customFieldDescribeResult.DecimalPlaces) {
+                customField.decimalPlaces = customFieldDescribeResult.DecimalPlaces;
+            }
+            
             customField.developerName = customFieldDescribeResult.DeveloperName;
             customField.label = customFieldDescribeResult.MasterLabel;
             customField.value = this.record[customFieldDescribeResult.DeveloperName];
+            customField.required = customFieldDescribeResult.Required ?? false;
             this.customFields.push(customField);
         }
 
@@ -62,5 +92,28 @@ export default class CmtRecordForm extends LightningElement {
 
     handleEdit() {
         this.readOnly = false;
+    }
+
+    handleSave() {
+        
+        let fieldComponents = [
+            ... this.template.querySelectorAll('lightning-input'), 
+            ... this.template.querySelectorAll('c-record-form-field')
+        ];
+
+        let invalidFields = [];
+
+        for(let fieldComponent of fieldComponents) {
+            if(!fieldComponent.checkValidity()) {
+                if(fieldComponent.customField) {
+                    invalidFields.push(fieldComponent.customField.label);
+                } else {
+                    invalidFields.push(fieldComponent.label);
+                }
+            }
+        }
+
+        console.log(invalidFields);
+        // do something with this.readOnly
     }
 }
--- force-app/main/default/lwc/recordFormField/recordFormField.js
+++ force-app/main/default/lwc/recordFormField/recordFormField.js
@@ -4,29 +4,53 @@ export default class RecordFormField extends LightningElement {
     @api customField;
     comboboxComponent;
     inputComponent;
-    inputRichTextComponent;
-    numberInputComponent;
+    inputDecimalComponent;
+    inputNumberComponent;
+    inputPhoneComponent;
+    inputTextComponent;
     @api readOnly;
+    textAreaComponent;
 
     connectedCallback() {
         switch(this.customField.componentType) {
-            case 'Input':
-                if(this.customField.formatter) {
-                    this.numberInputComponent = true;
-                } else {
-                    this.inputComponent = true;
-                }
+            case 'Input Decimal Component':
+               this.inputDecimalComponent = true;
+               break;
+            case 'Input Number Component':
+                this.inputNumberComponent = true;
                 break;
-
+            case 'Input Text Component':
+                this.inputTextComponent = true;
+                break;
+            case 'Input Phone Component':
+                this.inputPhoneComponent = true;
+                break;
+            case 'Input Component':
+                this.inputComponent = true;
+                break;
             case 'Combobox':
                 this.comboboxComponent = true;
                 break;
-            case 'Input Rich Text':
-                this.inputRichTextComponent = true;
+            case 'Text Area':
+                this.textAreaComponent = true;
         }
     }
 
     handleChange(event) {
+        console.log(event.detail.value);
+    }
 
+    @api checkValidity() {
+        let componentName;
+
+        if(this.comboboxComponent) {
+            componentName = 'lightning-combobox';
+        } else if(this.textAreaComponent) {
+            componentName = 'lightning-textarea';
+        } else {
+            componentName = 'lightning-input';
+        }
+
+        return this.template.querySelector(componentName).validity.valid;
     }
 }
